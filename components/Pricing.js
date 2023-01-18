ea952d9386107d618ee0b636327220f457bdd319
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2023-01-18 14:47:42 +0800

    feat: update price list content

--- components/Pricing.js
+++ components/Pricing.js
@@ -2,14 +2,12 @@ const Pricing = () => {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-10 mx-auto">
-        <div className="flex flex-col text-center w-full mb-20">
+        <div className="flex flex-col text-center w-full mb-10">
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">
-            Pricing
+            Price List
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Banh mi cornhole echo park skateboard authentic crucifix neutra
-            tilde lyft biodiesel artisan direct trade mumblecore 3 wolf moon
-            twee
+            Our Highly skilled barbers use quality straight razors and the best equipment to achieve the best haircut.
           </p>
         </div>
         <div className="lg:w-2/3 w-full mx-auto overflow-auto">
@@ -26,26 +24,36 @@ const Pricing = () => {
             </thead>
             <tbody>
               <tr>
-                <td className="px-4 py-3">Start</td>
-                <td className="px-4 py-3">5 Mb/s</td>
+                <td className="px-4 py-3">Adult Haircut</td>
+                <td className="px-4 py-3">RM 15</td>
               </tr>
               <tr>
-                <td className="border-t-2 border-gray-200 px-4 py-3">Pro</td>
-                <td className="border-t-2 border-gray-200 px-4 py-3">25 Mb/s</td>
+                <td className="border-t-2 border-gray-200 px-4 py-3">Kids Haircut (Under 12)</td>
+                <td className="border-t-2 border-gray-200 px-4 py-3">RM 8</td>
               </tr>
               <tr>
-                <td className="border-t-2 border-gray-200 px-4 py-3">Business</td>
-                <td className="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
-                  $50
-                </td>
+                <td className="border-t-2 border-gray-200 px-4 py-3">Head Massage</td>
+                <td className="border-t-2 border-gray-200 px-4 py-3">RM 10</td>
               </tr>
               <tr>
-                <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">
-                  Exclusive
-                </td>
-                <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
-                  $72
-                </td>
+                <td className="border-t-2 border-gray-200 px-4 py-3">Gold Mask Facial</td>
+                <td className="border-t-2 border-gray-200 px-4 py-3">RM 15</td>
+              </tr>
+              <tr>
+                <td className="border-t-2 border-gray-200 px-4 py-3">Black Mask</td>
+                <td className="border-t-2 border-gray-200 px-4 py-3">RM 10</td>
+              </tr>
+              <tr>
+                <td className="border-t-2 border-gray-200 px-4 py-3">Aloevera Mask</td>
+                <td className="border-t-2 border-gray-200 px-4 py-3">RM 20</td>
+              </tr>
+              <tr>
+                <td className="border-t-2 border-gray-200 px-4 py-3">Wash and Style</td>
+                <td className="border-t-2 border-gray-200 px-4 py-3">RM 10</td>
+              </tr>
+              <tr>
+                <td className="border-t-2 border-gray-200 px-4 py-3">Shave and Trim</td>
+                <td className="border-t-2 border-gray-200 px-4 py-3">RM 10</td>
               </tr>
             </tbody>
           </table>
